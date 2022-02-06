bdcf3fd01f7763c00a477f36ac0950418055a871
[Ahmet Albayrak] <[ahmetalperenalbayrak@gmail.com]>
2022-02-06 09:15:32 +0300

    Project fixed

--- app.js
+++ app.js
@@ -1,50 +1,55 @@
-const p1Button = document.querySelector('#p1Button')
-const p2Button = document.querySelector('#p2Button')
-const p1Display = document.querySelector('#p1Display')
-const p2Display = document.querySelector('#p2Display')
-const resetButton = document.querySelector('#reset')
-const winningScoreSelect = document.querySelector('#playto');
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
 
-let p1Score = 0;
-let p2Score = 0;
-let winningScore = 5;
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
+let winningScore = 3;
 let isGameOver = false;
 
-p1Button.addEventListener('click', () => {
-    if(!isGameOver) {
-        p1Score += 1;
-        if(p1Score === winningScore) {
+function updateScores(player, opponent) {
+    if (!isGameOver) {
+        player.score += 1;
+        if (player.score === winningScore) {
             isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+        player.display.textContent = player.score;
     }
-   
-        
-        p1Display.textContent = p1Score;
-    }
-   
+}
+
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2)
+})
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1)
 })
 
-p2Button.addEventListener('click', () => {
-    if(!isGameOver) {
-        p2Score += 1;
-        if(p2Score === winningScore) {
-            isGameOver = true;
-    }
-   
-    p2Display.textContent = p2Score; 
-        
-    }
-    
-})
 
-winningScoreSelect.addEventListener('change', () => {
+winningScoreSelect.addEventListener('change', function () {
     winningScore = parseInt(this.value);
+    reset();
 })
 
-resetButton.addEventListener('click' , () => {
+resetButton.addEventListener('click', reset)
+
+function reset() {
     isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
-})
-
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
+}
